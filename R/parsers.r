6bc96f66de6b0b3efaa202fd8c89ff2c4936252b
Raju Rimal <therimalaya@gmail.com>
2019-04-02 23:52:53 +0200

    Basic Structure Layed out

---
+++ R/parsers.r
@@ -0,0 +1,52 @@
+#' Parse a character string to a list
+#' @description These function parsers characters into list and
+#'   list into characters. They help to parse the argument and makes the
+#'   user input easier and intutive.
+#' @param chr A character string that represents a list or a vector
+#'   see Notes for details.
+#' @return A list or a vector
+#' @note A semicolon in the string separates the elements of a list while
+#'   A comma in the string separates the elements of a vector. See example.
+#' @examples
+#' chr2list("1, 2, 3") # c(1, 2, 3)
+#' chr2list("1, 2; 3, 4") # list(c(1, 2), c(3, 4))
+#' chr2list("1:2; 3:7") # list(1:2, 3:7)
+#' chr2list("1, 2, 3;") # list(c(1, 2, 3))
+#' @export
+chr2list <- function(chr) {
+  if (is.null(chr)) return(NULL)
+  x <- unlist(strsplit(chr, ";"))
+  x <- gsub("[[:space:]]", "", x)
+  ret <- lapply(x, function(y) {
+    if(grepl(":", y)) {
+      eval(parse(text = y))
+    } else {
+      as.numeric(unlist(strsplit(y, ",")))
+    }
+  })
+  if (!grepl(";", chr))
+    ret <- ret[[1]]
+  return(ret)
+}
+
+#' Parse List/Vector to a character string
+#' @param list A list or a vector which need to be deparsed as a string
+#' @return A character string representing the list
+#' @note This is the reverse for of @seealso chr2list
+#' @examples
+#' list2chr(c(1, 2, 3)) # "1, 2, 3"
+#' list2chr(list(c(1, 2), c(3, 4))) # "1, 2; 3, 4"
+#' list2chr(list(1:2, 3:7)) # "1:2; 3:7"
+#' @export
+list2chr <- function(list) {
+  if (is.null(list)) return(NULL)
+  depth <- depth(list)
+  if (depth > 2) {
+    stop("List should not nest more than 2 level deep.")
+  } else {
+    inner_chr <- sapply(list, paste0, collapse = ", ")
+    ret <- paste(inner_chr, collapse = "; ")
+  }
+  return(ret)
+}
+
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+suppressPackageStartupMessages(library(testthat))
+suppressPackageStartupMessages(library(simrel2))
+
+test_check("simrel2")
